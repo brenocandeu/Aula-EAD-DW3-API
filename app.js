e0ae86bb95e933414dd9396e4eca74a5fffd9818
brenocandeu <brenocandeu16@gmail.com>
2025-08-31 14:52:26 -0300

    Versão inicial do projeto com GET e POST

---
+++ app.js
@@ -0,0 +1,74 @@
+//@ Importa as bibliotecas
+const express = require('express');
+require('dotenv').config();
+
+//@ Configura o servidor
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+let salasDeAula = [
+  {
+    salasdeaulaid: 1,
+    descricao: 'Laboratório D04',
+    localizacao: 'Bloco D',
+    capacidade: 20,
+    removido: false,
+  },
+  {
+    salasdeaulaid: 2,
+    descricao: 'Laboratório D010',
+    localizacao: 'Bloco D',
+    capacidade: 25,
+    removido: false,
+  },
+  {
+    salasdeaulaid: 3,
+    descricao: 'Auditório',
+    localizacao: 'Bloco D',
+    capacidade: 100,
+    removido: true,
+  },
+];
+
+// Rota inicial
+app.get('/', (req, res) => {
+  res.send('API de Salas de Aula está funcionando!');
+});
+
+//Salas ativas
+app.get('/salas', (req, res) => {
+  const salasAtivas = salasDeAula.filter((sala) => sala.removido === false);
+  res.json(salasAtivas);
+});
+
+//Busca salas ativas pelo ID
+app.get('/salas/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const sala = salasDeAula.find(
+    (s) => s.salasdeaulaid === id && s.removido === false,
+  );
+
+  if (!sala) {
+    return res.status(404).json({ message: 'Sala de aula não encontrada.' });
+  }
+  res.json(sala);
+});
+
+// [POST] Insere uma nova sala
+app.post('/salas', (req, res) => {
+  const novaSala = req.body;
+  novaSala.salasdeaulaid = salasDeAula.length
+    ? Math.max(...salasDeAula.map((s) => s.salasdeaulaid)) + 1
+    : 1;
+  novaSala.removido = false;
+
+  salasDeAula.push(novaSala);
+  res.status(201).json(novaSala);
+});
+
+app.listen(port, () => {
+  console.log('Executando a aplicação ', process.env.APP_NAME);
+  console.log(`Servidor rodando na porta ${port}`);
+});
